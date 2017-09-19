f87c2d24d99b4f9d4c5329e5894864ba20d9bbc2
godlike007 <1530569806@qq.com>
2017-09-19 20:25:36 +0800

    add activity edit

---
+++ salesShare/js/common.js
@@ -0,0 +1,40 @@
+var Request = {
+    getData: function(ajaxParam) {
+        var promise = new Promise(function(resolve, reject) {
+            $.ajax({
+                url: ajaxParam.url + ajaxParam.param,
+                contentType: 'application/json',
+                dataType: 'json',
+                type: 'get',
+                async:ajaxParam.async||true,
+                //data: JSON.stringify(ajaxParam.param),
+                success: function(data) {
+                    resolve(data);
+                },
+                error: function(xhr, statusText) {
+                    reject(statusText);
+                }
+            });
+        });
+        return promise;
+    },
+    postData: function(ajaxParam) {
+        var promise = new Promise(function(resolve, reject) {
+            $.ajax({
+                url: ajaxParam.url,
+                contentType: 'application/json',
+                dataType: 'json',
+                type: 'post',
+                async:ajaxParam.async||true,
+                data: JSON.stringify(ajaxParam.param),
+                success: function(data) {
+                    resolve(data);
+                },
+                error: function(xhr, statusText) {
+                    reject(statusText);
+                }
+            });
+        });
+        return promise;
+    }	
+}
---
+++ salesShare/js/config.js
@@ -0,0 +1,20 @@
+var urlConfig = {
+    /*DataUrl : "http://soms-rest.jtljia.cn:90/services",
+    LoginUrl : "http://auth-rest.jtljia.cn:90/services",
+    DataBiUrl : "http://bi-rest.jtljia.cn:90/services",
+    PomsUrl: "http://poms-rest.jtljia.cn:90/",
+    appStatUrl: "http://poms-rest.jtljia.cn:90/app-project",
+    DocumentUrl: "http://poms-rest.jtljia.cn:90/document-admin",
+    SSOUrl: "http://sso.jtljia.cn:90",
+	UserPicUrl:"http://hr.goldmantis.com",*/
+	
+	/*SomsUrl : "http://172.21.52.29:8080/sub-station",
+    LoginUrl : "http://172.21.52.29:8889/services",
+    StatUrl: "http://172.21.52.29:18888/",
+
+	//StatUrl: "http://172.21.52.39:18888/",
+
+    DataUrl: "http://172.21.52.29:8887/",*/
+   
+    uploadImgUrl:"http://172.21.52.29:8887/"
+};
