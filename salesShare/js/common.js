9bcf50fcfd3590628c93654e43f5d7ba7f8e8ab2
godlike007 <1530569806@qq.com>
2017-10-06 11:23:49 +0800

    update

--- salesShare/js/common.js
+++ salesShare/js/common.js
@@ -39,3 +39,25 @@ var request = {
         return promise;
     }	
 }
+var uploadUtl = {
+	IMG:{
+		form_param_html:"<input type=\"text\" name=\"#name\" value=\"#name\"/>",
+		loadTool:function(obj;){
+			var _this = this;
+			var form_base_html ="<form enctype=\"multipart/form-data\" method=\"post\" name=\"#form_name\">#PARAM</form>";
+			
+			var param_list = [];
+			for(var o in obj.param){
+				var dom = "<input type=\"text\" name=\"#name\" value=\"#name\"/>";
+				dom = dom.replace(/#content/g,html);
+				param_list += 
+				
+    			var html = $("#froalaEditorWrap .fr-element").html();
+    			var dom = this.tem[st];
+
+					dom=dom.replace(/#id/g,this.handleTxtSourceIndex(csId,ht,stJqId))
+					dom=dom.replace(/#content/g,html);
+			}
+		}
+	}
+}
