aa08398858071eef07c1a56ae31557af7d301417
godlike007 <1530569806@qq.com>
2020-08-18 19:14:27 +0800

    1

---
+++ ele_editor/home/index.js
@@ -0,0 +1,102 @@
+
+
+base = new Vue({
+    el: "#base",
+    data() {
+        return {
+            section_list: [{
+                name: '字符串',
+            }, {
+                name: '数值',
+            }, {
+                name: 'set',
+            }, {
+                name: 'map',
+            }],
+
+            setting_data:{
+                '字符串':[{
+                    id:'_模板字符串',
+                    name:'模板字符串${}',
+                    hot:'item.field,value,fn()'
+                },
+                {
+                    id:'_模板字符串嵌套',
+                    name:'模板字符串${}嵌套',
+                    hot:''
+                },
+                {
+                    id:'_模板字符串本身',
+                    name:'模板字符串${}本身',
+                    hot:''
+                },{
+                    id:'_标签模板tag',
+                    name:'标签模板tag',
+                    hot:''
+                },{
+                    id:'_indexOf加强',
+                    name:'includes(), startsWith(), endsWith()',
+                    hot:''
+                },{
+                    id:'_repeat()',
+                    name:'repeat()',
+                    hot:''
+                }]
+            },
+
+
+            setting_list: [],
+            code:'',
+            handler_value_name:'',
+            cur_section:'字符串',
+            hot_use_list:[]
+        };
+    },
+
+    computed: {
+
+    },
+    mounted: function () {
+        this.init();
+        this.$nextTick(function () {
+
+        })
+    },
+
+    methods: {
+        init() {},
+        toggel_section(item) {
+            this.cur_section = item.name;
+            this.setting_list = this.setting_data[item.name];
+
+            this.$nextTick(function () {
+              
+            })
+
+        },
+        add_code(item) {
+            this.code += $.trim($("#"+item.id).html().replace(/\#/,this.handler_value_name))+"\n";
+
+            this.$nextTick(function () {
+              
+            })
+
+        },
+        toggel_hot_use(item) {
+            debugger;
+
+            this.hot_use_list = item.hot.split(",");
+
+        },
+        assign_value(item) {
+            this.handler_value_name = item;
+
+
+        },
+    }
+});
+
+
+$(function () {
+
+})
